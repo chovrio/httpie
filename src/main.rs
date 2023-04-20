a6c38ba4286f09e1261e9ac91c25f4575e0c8250
chovrio <1480733667@qq.com>
2023-04-20 12:18:25 +0800

    feat: over but there is a small problem
    correct it later when I have the ability.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
-use anyhow::Result;
-use clap::{Parser, Subcommand};
-use reqwest::Url;
+use anyhow::{anyhow, Ok, Result};
+use clap::{ Parser};
+use colored::Colorize;
+use mime::Mime;
+use reqwest::{Client, Url,Response, header};
+use std::{str::FromStr, collections::HashMap};
+
 // 定义 HTTPie的CLI的主入口，它包含若干个子命令
 // 下面的///的注释是文档 clap 会将其作为 CLI 的帮助
-
 // A native httpie implementation with Rust，can you imagine how easy it is?
 #[derive(Debug, Parser)]
 #[clap(version = "1.0", author = "chovrio")]
@@ -21,23 +24,134 @@ enum SubCommond {
 #[derive(Parser, Debug)]
 struct Get {
     /// HTTP请求的URL
-    #[clap(parse(try_from_str = parse_url))]
+    // #[clap(parse(try_from_str = parse_url))]
     url: String,
-}
-#[derive(Parser, Debug)]
-struct Post {
-    /// HTTP请求的URL
-    url: String,
-    /// HTTP请求的body
-    body: Vec<String>,
-}
-fn main() {
-    let opts: Opts = Opts::parse();
-    println!("{:?}", opts);
 }
 
 fn parse_url(s: &str) -> Result<String> {
     // 这里我们仅仅检查一下 URL 是否合法
     let _url: Url = s.parse()?;
     Ok(s.into())
+}
+
+#[derive(Parser, Debug)]
+struct Post {
+    /// HTTP请求的URL
+    // #[clap(parse(try_from_str = parse_url))]
+    url: String,
+    /// HTTP 请求的 body
+    // #[clap(parse(try_from_str =parse_kv_pair))]
+    body: Vec<KvPair>,
+}
+/// 命令行中的 key = value 可以通过 parse_kv_pair 解析成 KvPair 结构
+#[derive(Debug,PartialEq)]
+#[derive(Clone)]
+struct KvPair {
+    k: String,
+    v: String,
+}
+/// 当我们实现FormStr trait后 可以用 str.parse() 方法将字符串解析成 KvPair
+impl FromStr for KvPair {
+    type Err = anyhow::Error;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        // 使用 = 进行 split 这会得到一个迭代器
+        let mut split = s.split("=");
+        let err = || anyhow!(format!("Failed to parse {}", s));
+        Ok(Self {
+            // 从迭代器中取第一个结果作为key，迭代器返回Some(T)/None
+            //   我们将其转换成Ok(T)/Err(E),然后用?处理错误
+            k: (split.next().ok_or_else(err)?).to_string(),
+            // 从迭代器中取第二个结果作为value
+            v: (split.next().ok_or_else(err)?).to_string(),
+        })
+    }
+}
+fn parse_kv_pair(s: &str) -> Result<KvPair> {
+    Ok(s.parse()?)
+}
+async fn get(client:Client,args:&Get) -> Result<()>{
+    let resp = client.get(&args.url).send().await?;
+    Ok(print_resp(resp).await?)
+}
+async fn post(client:Client,args:&Post) -> Result<()>{
+    let mut body = HashMap::new();
+    for pair in args.body.iter() {
+        body.insert(&pair.k, &pair.v);
+    }
+    let resp = client.post(&args.url).json(&body).send().await?;
+    Ok(print_resp(resp).await?)
+}
+// 打印服务器版本号 + 状态码
+fn print_status(resp:&Response) {
+    let status = format!("{:?} {}",resp.version(), resp.status()).blue();
+    println!("{}\n", status);
+}
+// 打印服务器返回的 HTTP headers
+fn print_headers(resp:&Response) {
+    for (name,value) in resp.headers() {
+        println!("{}：{:?}", name.to_string().green(),value);
+    }
+}
+// 打印服务器返回的HTTP body
+fn print_body(m:Option<Mime>,body:&String) {
+    match m {
+        // 对于 "application/json" 我们 pretty print
+        Some(v) if v == mime::APPLICATION_JSON => {
+            println!("{}", jsonxf::pretty_print(body).unwrap().cyan());
+        },
+        // 其他mime type 就直接输出
+        _ => println!("{}", body),
+    }
+}
+// 打印整个响应
+async fn print_resp(resp:Response) -> Result<()>{
+    print_status(&resp);
+    print_headers(&resp);
+    let mime = get_content_type(&resp);
+    let body = resp.text().await?;
+    print_body(mime, &body);
+    Ok(())
+}
+fn get_content_type(resp:&Response) -> Option<Mime> {
+    resp.headers().get(header::CONTENT_TYPE)
+    .map(|v| v.to_str().unwrap().parse().unwrap())
+}
+// 程序的入口函数 因为在http请求时我们使用了异步处理，所以引入Tokio
+#[tokio::main]
+async fn main() -> Result<()> {
+    let opts: Opts = Opts::parse();
+    let mut headers = header::HeaderMap::new();
+    // 为我们的 HTTP 客户端添加一些缺省的 HTTP 头
+    headers.insert("X-POWERED-BY", "Rust".parse()?);
+    headers.insert(header::USER_AGENT, "Rust Httpie".parse()?);
+    let client = reqwest::Client::builder().default_headers(headers).build()?;
+    let result = match opts.subcmd {
+        SubCommond::Get(ref args) => get(client, args).await?,
+        SubCommond::Post(ref args) => post(client, args).await?,
+    };
+    Ok(result)
+}
+
+// 仅在cargo test 时才编译
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn parse_url_works() {
+        assert!(parse_url("abc").is_err());
+        assert!(parse_url("http://abc.xyz").is_ok());
+        assert!(parse_url("https://bin.org/post").is_ok());
+    }
+    #[test]
+    fn parse_kv_works() {
+        assert!(parse_kv_pair("a").is_err());
+        assert_eq!(parse_kv_pair("a=1").unwrap(),KvPair {
+            k:"a".into(),
+            v:"1".into()
+        });
+        assert_eq!(parse_kv_pair("b=").unwrap(),KvPair {
+            k:"b".into(),
+            v:"".into()
+        })
+    }
 }
